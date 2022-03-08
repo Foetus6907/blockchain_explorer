76ff71763449f16971ad6e7c0c2cda54ffb2b166
Foetus6907 <mghoubali@gmail.com>
2022-03-08 15:42:53 +0100

    add Quasar css framework

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { minifyHtml, injectHtml } from 'vite-plugin-html';
 
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   const envMap = loadEnv(mode, process.cwd());
   return defineConfig({
     plugins: [
-      vue(),
+      vue({
+        template: { transformAssetUrls }
+      }),
+      quasar({
+        sassVariables: 'src/quasar-variables.sass'
+      }),
       minifyHtml(),
       injectHtml({
         data: {
