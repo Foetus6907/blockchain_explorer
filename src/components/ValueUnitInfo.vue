4ab890a66d784156bea5ea3342d7b8bb5d674edb
Foetus6907 <mghoubali@gmail.com>
2022-03-11 16:44:33 +0100

    Add ValueUnitInfo.vue component + fix vue type definition

---
+++ src/components/ValueUnitInfo.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+import Block from "../../core/domaine/model/Block";
+const props = defineProps({
+  unit: String,
+  value: Number,
+  withUnit: Boolean,
+  formatNumberUs: Boolean
+});
+
+const val = computed(() => {
+  let val = "";
+  if (props.formatNumberUs) {
+   val = Block.convertNumberUs(props.value)
+  }
+  if (props.withUnit) {
+    val = `${val} ${props.unit}`
+  }
+  return val
+})
+
+</script>
+<template>
+  <span>{{val}}</span>
+</template>
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,13 @@
-/*declare module "*.vue" {
+declare module "*.vue" {
   import { defineComponent } from "vue";
   const Component: ReturnType<typeof defineComponent>;
   export default Component;
-}*/
+}
+/*
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+ */
