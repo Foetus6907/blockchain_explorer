439100580f1ae97a47263b6442baa29c12399b59
Foetus6907 <mghoubali@gmail.com>
2022-03-14 14:16:06 +0100

    add transaction pagination

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,6 +9,7 @@ import BlockTransactionListItems from "./BlockTransactionListItems.vue";
 const blockUseCase = inject(BitcoinChainUseCaseKey);
 
 const isLoading = ref(false);
+const currentPage = ref(1);
 
 const blockHash: Ref<string> = ref("00000000000000000007878ec04bb2b2e12317804810f4c26033585b3f81ffaa");
 const block: Ref<Block | null> = ref(null);
@@ -22,10 +23,28 @@ const searchBlock = async (e: any) => {
     isLoading.value = true;
     if (blockUseCase) {
       block.value = await blockUseCase.getBlockFromHash(blockHash.value);
+      currentPage.value = 1
     }
     isLoading.value = false;
   }
 };
+
+const chunk = (chunkSize, arrayToChunk) => {
+  var R = [];
+  for (let i = 0; i < arrayToChunk.length; i += chunkSize)
+    R.push(arrayToChunk.slice(i, i + chunkSize));
+  return R;
+}
+
+const nbrTransactionPerPage =  5;
+const maxPage = computed(() => {
+  return Math.round(transactions.value.length / 5)
+})
+
+const paginatedTransactions = computed(() => {
+  return chunk(nbrTransactionPerPage, transactions.value);
+});
+
 </script>
 
 <template>
@@ -45,15 +64,28 @@ const searchBlock = async (e: any) => {
       </q-card-section>
       <template v-if="block !== null">
         <BlockHeader :block="block" />
-        <q-card-section v-if="block !== null">
+        <q-card-section >
           <q-list separator>
             <q-item class="block">
-              <BlockTransactionListItems v-for="(transaction, key) in transactions" :key="key"
+              <BlockTransactionListItems v-for="(transaction, key) in paginatedTransactions[currentPage]" :key="key"
                                          :transaction="transaction" />
             </q-item>
             <q-separator />
           </q-list>
         </q-card-section>
+        <div class="q-pa-lg flex flex-center">
+          <q-pagination
+            v-model="currentPage"
+            :max="maxPage"
+            direction-links
+            boundary-links
+            input
+            icon-first="skip_previous"
+            icon-last="skip_next"
+            icon-prev="fast_rewind"
+            icon-next="fast_forward"
+          />
+        </div>
       </template>
     </q-card>
   </q-page>
